9860f85d850158866b2951c43e990c8948f67099
Gustavo <gustavosilva90p@gmail.com>
2026-03-16 07:22:54 -0300

    Pasta API-MAIN DUPLICADA, removi as coisas duplicadas e adicionei oq era novo a pasta principal

--- API-main/backend/src/db/connect.js
+++
@@ -1,9 +0,0 @@
-import dbconfig from './dbconfig.js'; 
-
-dbconfig.query('SELECT * FROM users', (err, results) => {
-    if (err) {
-        console.error('Error executing query:', err);
-        return;
-    }
-    console.log('Query results:', results);
-});
